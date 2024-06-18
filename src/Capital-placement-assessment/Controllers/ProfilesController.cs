f6a45c739db62747b3786f9cff5be837d78bc0fa
donkennie <ajeigbekehinde160@gmail.com>
2024-06-18 16:25:00 +0100

    Create project structure template

---
+++ src/Capital-placement-assessment/Controllers/ProfilesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Capital_placement_assessment.Controllers
+{
+    [Route("[controller]")]
+    public class ProfilesController : Controller
+    {
+        private readonly ILogger<ProfilesController> _logger;
+
+        public ProfilesController(ILogger<ProfilesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ src/Capital-placement-assessment/Repositories/Implementation/ProfileRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capital_placement_assessment.Repositories.Implementation
+{
+    public class ProfileRepository
+    {
+        
+    }
+}
