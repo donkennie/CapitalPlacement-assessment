154fe57999346450dc3c829197cb3e568ae358a9
donkennie <ajeigbekehinde160@gmail.com>
2024-07-26 14:03:28 +0100

    make changes

---
+++ src/Capital-placement-assessment/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Capital_placement_assessment.Models
+{
+    public class Person(string firstName, string lastname, DateTime dateOfBirth, string gender)
+    {
+        public string FirstName { get; protected set; } = firstName;
+        public string LastName { get; protected set; } = lastname;
+        public DateTime DateOfBirth { get; protected set; } = dateOfBirth;
+        public string Gender { get; protected set;} = gender;
+
+        public static Person CreatePersonFactory(PersonDTO personDTO)
+        {
+           
+            return new Person(personDTO.firstName, personDTO.lastname, 
+                              personDTO.dateOfBirth, personDTO.gender);
+        }
+        
+    }
+
+
+    
+    public record PersonDTO(int id, string firstName, string lastname, DateTime dateOfBirth, string gender);
+
+}
